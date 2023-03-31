aab660cba80dab159f7644c33761f8231c0eda1e
Wandson Emanuel <wandson7emanuel@hotmail.com>
2023-03-31 14:44:43 -0300

    Resolução de Cálculos

--- LógicaCálculo.js
+++ LógicaCálculo.js
@@ -2,7 +2,7 @@ function calcularConstante() {
     var ds = parseFloat(document.getElementById("ds").value);
     var jst = parseFloat(document.getElementById("jst").value);
     var ist = parseFloat(document.getElementById("ist").value);
-    var km = ((ds * jst) / (24 - ist)).toFixed(2);
+    var km = ((ds/jst)*ist);
     document.getElementById("km").innerHTML = km;
 }
 
@@ -12,12 +12,12 @@ function calcularTHE() {
     var pcad = parseInt(document.getElementById("pcad").value);
     var pcsi = parseInt(document.getElementById("pcsi").value);
     var pcit = parseInt(document.getElementById("pcit").value);
-    var the = (pcm + pci + pcad + pcsi + pcit);
+    var the = ((pcm*4)+(pci*6)+(pcad*10)+(pcsi*10)+(pcit*18));
     document.getElementById("the").innerHTML = the;
 }
 function calcularQP() {
     var the = parseInt(document.getElementById("the").innerHTML);
     var km = parseFloat(document.getElementById("km").innerHTML);
-    var qp = Math.ceil(the / km);
+    var qp = Math.ceil(the*km);
     document.getElementById("qp").innerHTML = qp;
     }
